3db077a0487409d4f09b6d89890c6be8c1efe16f
D.Senal Danushka Galagedara <138513885+senalgalagedara@users.noreply.github.com>
2023-07-08 22:12:49 +0530

    Add files via upload

---
+++ test11/Form1.Designer.vb
@@ -0,0 +1,401 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        ListBox1 = New ListBox()
+        ListBox2 = New ListBox()
+        Label6 = New Label()
+        Label7 = New Label()
+        ListBox3 = New ListBox()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        ListBox4 = New ListBox()
+        ListBox5 = New ListBox()
+        Button1 = New Button()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        CheckBox5 = New CheckBox()
+        CheckBox6 = New CheckBox()
+        CheckBox7 = New CheckBox()
+        CheckBox8 = New CheckBox()
+        CheckBox9 = New CheckBox()
+        CheckBox10 = New CheckBox()
+        CheckBox11 = New CheckBox()
+        CheckBox12 = New CheckBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(45, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(83, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "සම්පුර්න නම"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(134, 24)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(357, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(45, 92)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(109, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "පදිචියේ ස්වභාවය"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(45, 135)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(112, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "අධ්‍යාපන සුදුසුකම්"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(45, 178)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(107, 15)
+        Label5.TabIndex = 12
+        Label5.Text = "වෘත්තිය සුදුසුකම්"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Items.AddRange(New Object() {"Certificate Level", "Diploma Level", "Degree"})
+        ListBox1.Location = New Point(175, 178)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(114, 34)
+        ListBox1.TabIndex = 13
+        ' 
+        ' ListBox2
+        ' 
+        ListBox2.FormattingEnabled = True
+        ListBox2.ItemHeight = 15
+        ListBox2.Items.AddRange(New Object() {"කුඹුරු වතු", "පොල් වතු", "රබර් වතු", "තේ වතු", "කුලි ඉඩම්", "None"})
+        ListBox2.Location = New Point(145, 324)
+        ListBox2.Name = "ListBox2"
+        ListBox2.Size = New Size(91, 34)
+        ListBox2.TabIndex = 18
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(45, 325)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(84, 15)
+        Label6.TabIndex = 19
+        Label6.Text = "දේපල විස්තර"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(308, 324)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(176, 15)
+        Label7.TabIndex = 20
+        Label7.Text = "දේපල වලින්  දළ ස්ථිර ආදායම"
+        ' 
+        ' ListBox3
+        ' 
+        ListBox3.FormattingEnabled = True
+        ListBox3.ItemHeight = 15
+        ListBox3.Items.AddRange(New Object() {"0-25000", "25000-50000", "50000-75000", "75000-100000", "100000+", "None"})
+        ListBox3.Location = New Point(505, 324)
+        ListBox3.Name = "ListBox3"
+        ListBox3.Size = New Size(96, 34)
+        ListBox3.TabIndex = 21
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(45, 387)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(168, 15)
+        Label8.TabIndex = 22
+        Label8.Text = "මත්පැන් හා දුම්වැටි භාවිතය"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(45, 436)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(121, 15)
+        Label9.TabIndex = 25
+        Label9.Text = "භාවිතයේ ස්වභාවය"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(217, 436)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(46, 15)
+        Label10.TabIndex = 26
+        Label10.Text = "දුම්වැටි"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(432, 436)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(55, 15)
+        Label11.TabIndex = 27
+        Label11.Text = "මත්පැන්"
+        ' 
+        ' ListBox4
+        ' 
+        ListBox4.FormattingEnabled = True
+        ListBox4.ItemHeight = 15
+        ListBox4.Items.AddRange(New Object() {"Daily", "Weekly", "Monthly"})
+        ListBox4.Location = New Point(279, 436)
+        ListBox4.Name = "ListBox4"
+        ListBox4.Size = New Size(116, 34)
+        ListBox4.TabIndex = 28
+        ' 
+        ' ListBox5
+        ' 
+        ListBox5.FormattingEnabled = True
+        ListBox5.ItemHeight = 15
+        ListBox5.Items.AddRange(New Object() {"Daily", "Weekly", "Monthly"})
+        ListBox5.Location = New Point(505, 436)
+        ListBox5.Name = "ListBox5"
+        ListBox5.Size = New Size(121, 34)
+        ListBox5.TabIndex = 29
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(338, 498)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(175, 54)
+        Button1.TabIndex = 30
+        Button1.Text = "Submit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(175, 91)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(55, 19)
+        CheckBox1.TabIndex = 31
+        CheckBox1.Text = "ස්ථිර"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(249, 91)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(47, 19)
+        CheckBox2.TabIndex = 32
+        CheckBox2.Text = "කුලී"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(320, 91)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(122, 19)
+        CheckBox3.TabIndex = 33
+        CheckBox3.Text = "දෙමාපියන් සමග"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(175, 134)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(80, 19)
+        CheckBox4.TabIndex = 34
+        CheckBox4.Text = "G.C.E. O/L"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(276, 134)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(79, 19)
+        CheckBox5.TabIndex = 35
+        CheckBox5.Text = "G.C.E. A/L"
+        CheckBox5.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox6
+        ' 
+        CheckBox6.AutoSize = True
+        CheckBox6.Location = New Point(379, 134)
+        CheckBox6.Name = "CheckBox6"
+        CheckBox6.Size = New Size(63, 19)
+        CheckBox6.TabIndex = 36
+        CheckBox6.Text = "Degree"
+        CheckBox6.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox7
+        ' 
+        CheckBox7.AutoSize = True
+        CheckBox7.Location = New Point(45, 269)
+        CheckBox7.Name = "CheckBox7"
+        CheckBox7.Size = New Size(127, 19)
+        CheckBox7.TabIndex = 37
+        CheckBox7.Text = "පෙම්වතෙක් සිටියි"
+        CheckBox7.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox8
+        ' 
+        CheckBox8.AutoSize = True
+        CheckBox8.Location = New Point(192, 269)
+        CheckBox8.Name = "CheckBox8"
+        CheckBox8.Size = New Size(126, 19)
+        CheckBox8.TabIndex = 38
+        CheckBox8.Text = "පෙම්වතෙක් නැත"
+        CheckBox8.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox9
+        ' 
+        CheckBox9.AutoSize = True
+        CheckBox9.Location = New Point(337, 269)
+        CheckBox9.Name = "CheckBox9"
+        CheckBox9.Size = New Size(119, 19)
+        CheckBox9.TabIndex = 39
+        CheckBox9.Text = "පෙම්වතියක් සිටි"
+        CheckBox9.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox10
+        ' 
+        CheckBox10.AutoSize = True
+        CheckBox10.Location = New Point(488, 269)
+        CheckBox10.Name = "CheckBox10"
+        CheckBox10.Size = New Size(128, 19)
+        CheckBox10.TabIndex = 40
+        CheckBox10.Text = "පෙම්වතියක් නැත"
+        CheckBox10.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox11
+        ' 
+        CheckBox11.AutoSize = True
+        CheckBox11.Location = New Point(245, 386)
+        CheckBox11.Name = "CheckBox11"
+        CheckBox11.Size = New Size(45, 19)
+        CheckBox11.TabIndex = 41
+        CheckBox11.Text = "ඔව්"
+        CheckBox11.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox12
+        ' 
+        CheckBox12.AutoSize = True
+        CheckBox12.Location = New Point(308, 386)
+        CheckBox12.Name = "CheckBox12"
+        CheckBox12.Size = New Size(54, 19)
+        CheckBox12.TabIndex = 42
+        CheckBox12.Text = "නැත"
+        CheckBox12.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(833, 586)
+        Controls.Add(CheckBox12)
+        Controls.Add(CheckBox11)
+        Controls.Add(CheckBox10)
+        Controls.Add(CheckBox9)
+        Controls.Add(CheckBox8)
+        Controls.Add(CheckBox7)
+        Controls.Add(CheckBox6)
+        Controls.Add(CheckBox5)
+        Controls.Add(CheckBox4)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button1)
+        Controls.Add(ListBox5)
+        Controls.Add(ListBox4)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(ListBox3)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(ListBox2)
+        Controls.Add(ListBox1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents ListBox5 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox6 As CheckBox
+    Friend WithEvents CheckBox7 As CheckBox
+    Friend WithEvents CheckBox8 As CheckBox
+    Friend WithEvents CheckBox9 As CheckBox
+    Friend WithEvents CheckBox10 As CheckBox
+    Friend WithEvents CheckBox11 As CheckBox
+    Friend WithEvents CheckBox12 As CheckBox
+End Class
---
+++ test11/Form1.vb
@@ -0,0 +1,140 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim name As String
+
+        Dim sthira As String
+        Dim kuli As String
+        Dim ammala As String
+
+        Dim ol As String
+        Dim al As String
+        Dim degree As String
+
+        Dim occupation As String
+
+        Dim kollekinnw As String
+        Dim kollekna As String
+        Dim kellekinnw As String
+        Dim kellekna As String
+
+        Dim depala As String
+        Dim adayama As String
+
+        Dim yes As String
+        Dim no As String
+
+        Dim ela As String
+        Dim ew As String
+        Dim hpo As String
+
+        Dim s As String
+        Dim l As String
+
+
+
+        occupation = ListBox1.SelectedItem
+        depala = ListBox2.SelectedItem
+        adayama = ListBox3.SelectedItem
+        ew = ListBox4.SelectedItem
+        hpo = ListBox5.SelectedItem
+
+
+
+
+
+
+        Dim x(3, 8) As String
+
+        x(0, 0) = "G.C.E. A/L කරලා Degree එකක් කරන්න ඔනේ or private job එකක් කරන්න ඔනේ."
+        x(1, 0) = "Gov Campus or Private Campus ගිහින් degree එකක් කරන්න ඔනේ or වෘත්තිය සුදුසුකමක් හදාගන්න ඔනේ. "
+        x(2, 0) = "Finally හොද job එකක් කරන්න පුලුවන්."
+
+        x(0, 1) = "නිවස තමාගේ වන නිසා නිවසේ නඩත්තු වියදම් හැර වෙනත් වියදමි නැත."
+        x(1, 1) = "මාසිකව කුලි ගාස්තුවක් ගෙවිය යුතුය (about 40000 LKR monthly)"
+        x(2, 1) = "තමාගේම නිවසක් සාදාගත යුතුය (about 50 lks at least) or කුලි නිවසකට යායුතුය (about 40000LKR monthly)"
+
+        x(0, 2) = "කෙල්ලෙක් හොයාගන්න ඔනේ, දැනටම කෙල්ලෙක් නැ කියන්නේ උබ කැතයි. ඒනිසා පෙනුම ගැන හිතන්න ඔනේ."
+        x(1, 2) = "කොල්ලෙක් හොයාගන්න ඔනේ, දැනටම කොල්ලෙක් නැ කියන්නේ උබ කැතයි. ඒනිසා පෙනුම ගැන හිතන්න ඔනේ."
+
+        x(0, 3) = "කෙල්ලෙක් ඉන්නවනම් එයාට gifts අරන් දෙන්න ඔන, ඇවිදින්න ඔන, කසාද බදින්න ඔන , ඊට පස්සෙ පවුල් වියදම්, එයාගෙ දෙමාපියන් බලාගන්නත් ඔනේ."
+        x(1, 3) = "කොල්ලෙක් ඉන්නවනම් එයාව කසාද බදින්න ඔන, Dowry හදාගන්න ඔන, කොල්ලට සල්ලි නැත්නම් වෙන එකකට මාරුවෙන්න ඔනේ or එයත් එක්ක ගොඩ යන්න ඔනේ."
+
+        x(0, 4) = "දේපල තිබේ නම් එවා නඩත්තු කරන්න ඔනේ, ලැබෙන ආදායම වැඩිකර ගන්න ඔනේ."
+        x(1, 4) = "දේපල නැත්නම් දේපල එකතු කරන්න ඔනේ or නිකන් job එකේ ආදායමෙන් ජිවත් වෙන්න ඔනේ."
+
+        x(0, 5) = "1000*30 = 30000 lkr for month"
+        x(1, 5) = "125*30 = 3750 for month"
+
+        x(0, 6) = "3000*12 = 36000lkr for year"
+        x(1, 6) = "125*12 = 1500 for year"
+
+        x(0, 7) = "12000 *1 = 12000 for year"
+        x(1, 7) = "125*1 = 125 for year"
+
+        name = TextBox1.Text
+
+
+        If CheckBox1.Checked Then
+            sthira = x(0, 1)
+        ElseIf CheckBox2.Checked Then
+            kuli = x(1, 1)
+        ElseIf CheckBox3.Checked Then
+            ammala = x(2, 1)
+        End If
+        If CheckBox4.Checked Then
+            ol = x(0, 0)
+        ElseIf CheckBox5.Checked Then
+            al = x(1, 0)
+        ElseIf CheckBox6.Checked Then
+            degree = x(2, 0)
+        End If
+        If CheckBox7.Checked Then
+            kollekinnw = x(1, 3)
+        ElseIf CheckBox8.Checked Then
+            kollekna = x(1, 2)
+        ElseIf CheckBox9.Checked Then
+            kellekinnw = x(0, 3)
+        ElseIf CheckBox10.Checked Then
+            kellekna = x(0, 2)
+        End If
+        If CheckBox11.Checked Then
+            If ew = ("Daily") Then
+                s = x(1, 5)
+            ElseIf ew = ("Monthly") Then
+                s = x(1, 6)
+            ElseIf ew = ("yearly") Then
+                s = x(1, 7)
+            End If
+
+            If hpo = ("Daily") Then
+                l = x(0, 5)
+            ElseIf hpo = ("Monthly") Then
+                l = x(0, 6)
+            ElseIf hpo = ("yearly") Then
+                l = x(0, 7)
+            End If
+
+
+        ElseIf CheckBox12.Checked Then
+            ela = ("සුබ පැතුම්!, ඔබ මාසයකට රු. 35000ක් පමන ඉතුරු කරනවා ")
+
+        End If
+
+
+        MsgBox("hi " & name & vbNewLine &
+               "Thank you for using Life Solution Software" & vbNewLine &
+               "මෙන්න ඔබගේ ඉදිරි අවුරුදු 10ට එන ප්‍රශ්න!" & vbNewLine &
+               ol & al & degree & vbNewLine &
+               "ඔබගේ වෘත්තිය සුදුසුකම වන්නේ " & occupation & vbNewLine &
+               sthira & kuli & ammala & vbNewLine &
+               kollekinnw & kollekna & kellekinnw & kellekna & vbNewLine &
+               "ඔබට අයත් දේපල " & depala & "හා ආදායම " & adayama & vbNewLine &
+               yes & no & vbNewLine &
+               "ඔබට මත්ද්‍රවය සදහා යන වියදම් " & s & l)
+
+
+    End Sub
+
+
+End Class
